fd16646a45f4dd48b7d1656ecb4097976985677b
Salman Ali <92622212+salmanali8@users.noreply.github.com>
2023-11-24 23:57:34 +0500

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,28 @@
+pip install transformers streamlit
+import streamlit as st
+from transformers import pipeline
+
+# Load pre-trained model for text classification
+classifier = pipeline("sentiment-analysis")
+
+# Streamlit app
+def main():
+    st.title("Text Classification App")
+
+    # User input for text
+    text_input = st.text_area("Enter text for classification:")
+
+    if st.button("Classify"):
+        if text_input:
+            # Perform classification
+            result = classifier(text_input)
+            
+            # Display result
+            st.write("### Classification Result:")
+            for res in result:
+                st.write(f"Label: {res['label']}, Confidence: {res['score']}")
+        else:
+            st.warning("Please enter some text.")
+
+if __name__ == "__main__":
+    main()
